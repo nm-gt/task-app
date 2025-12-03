77934414950b5d95847d606c18d7a759ac1cfba5
nima <lf.nima2017@gmail.com>
2025-12-03 13:45:35 +0330

    update-form

--- src/app/pages/signup/signup.ts
+++ src/app/pages/signup/signup.ts
@@ -18,6 +18,7 @@ export class Signup {
   name = new FormControl('', [Validators.required, Validators.minLength(2)]);
   lastname = new FormControl('', [Validators.required, Validators.minLength(2)]);
   email = new FormControl('', [Validators.required, Validators.email]);
+  phone = new FormControl(null, [Validators.required, Validators.pattern(/^09\d{9}$/)])
   form = new FormGroup({
     password: new FormControl('', [
       Validators.required,
@@ -35,6 +36,12 @@ export class Signup {
       return password === confirm ? null : { passwordMismatch: true };
     }
   });
+
+  show = false;
+
+  togglePassword() {
+    this.show = !this.show;
+  }
 
   onSubmitClicked(e: Event) {
     e.preventDefault();
@@ -79,4 +86,52 @@ export class Signup {
   doLogout() {
     this.auth.logout();
   }
+
+  conditions = {
+    length: false,
+    uppercase: false,
+    special: false,
+    number: false
+  };
+
+  updateConditions() {
+    const pass = this.form.get('password')?.value || '';
+
+    this.conditions.length = pass.length >= 8;
+    this.conditions.uppercase = /[A-Z]/.test(pass);
+    this.conditions.special = /[!@#$%^&*()_+\-=]/.test(pass);
+    this.conditions.number = /[0-9]/.test(pass);
+  }
+
+  getLastPassedIndex(): number {
+    const arr = [
+      this.conditions.length,
+      this.conditions.uppercase,
+      this.conditions.special,
+      this.conditions.number
+    ];
+    return arr.lastIndexOf(true);
+  }
+
+  getColorFor(condition: boolean): string {
+    if (condition) return 'green';
+    return '#e0e0e0';
+  }
+
+  getPassedCount(): number {
+    return Object.values(this.conditions).filter(c => c).length;
+  }
+
+  getBoxColor(index: number): string {
+    const passed = this.getPassedCount();
+
+    if (passed === 0) return index === 0 ? 'red' : '#e0e0e0';
+    if (passed === 1) return index < 1 ? 'orange' : '#e0e0e0';
+    if (passed === 2) return index < 2 ? '#ffcc00' : '#e0e0e0';
+    if (passed === 3) return index < 3 ? '#7cd97c' : '#e0e0e0';
+    if (passed === 4) return 'green';
+
+    return '#e0e0e0';
+  }
+
 }
